ed68efbc832dacb8d57ee4a0e347b463c2dadd31
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2024-12-14 21:58:39 +0300

    u

--- SUKOON-APP/src/customComponents/login/login.tsx
+++ SUKOON-APP/src/customComponents/login/login.tsx
@@ -1,11 +1,22 @@
+import { Heading } from '@chakra-ui/react'
 import React from 'react'
+import BackgroundImageWhite from '@/images/Screenshot 2024-12-14 214554.png'
 
 const login = () => {
   return (
-    <div>
-      Hello
+    <div 
+      style={{
+        backgroundImage: `url(${BackgroundImageWhite})`, // Set background image
+        backgroundRepeat: 'repeat', // Repeat the image
+        backgroundPosition: 'top left', // Start repeating from the top-left corner
+        height: '100vh', // Make the div fill the entire viewport height
+      }}
+    >
+      <Heading color={'black'}>
+        Hello
+      </Heading>
     </div>
   )
 }
 
-export default login
+export default login;
