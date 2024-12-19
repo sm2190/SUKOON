6d49936355ee483d8c1c97923006eaf3132cccd4
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2024-12-20 00:48:18 +0300

    u

--- SUKOON-APP/vite.config.ts
+++ SUKOON-APP/vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/SUKOON/',  // Adjust base path for GitHub Pages
   plugins: [react(), tsconfigPaths()],
 })
