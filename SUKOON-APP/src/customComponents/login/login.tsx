f6548ae62d1cd7cae63eb05899afc42b5f3a12c7
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2024-12-19 23:54:45 +0300

    Started login

--- SUKOON-APP/src/customComponents/login/login.tsx
+++ SUKOON-APP/src/customComponents/login/login.tsx
@@ -1,20 +1,36 @@
-import { Heading } from '@chakra-ui/react'
+import { Button, Flex, Heading, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
-import BackgroundImageWhite from '@/images/Screenshot 2024-12-14 214554.png'
+import './login.css'
+import LoginAnimation from '@/images/animatedIcons/Forest-Waterfall.mp4'
 
 const login = () => {
   return (
-    <div 
-      style={{
-        backgroundImage: `url(${BackgroundImageWhite})`, // Set background image
-        backgroundRepeat: 'repeat', // Repeat the image
-        backgroundPosition: 'top left', // Start repeating from the top-left corner
-        height: '100vh', // Make the div fill the entire viewport height
-      }}
-    >
-      <Heading color={'black'}>
-        Hello
-      </Heading>
+    <div>
+
+      <Stack className='loginStartStack'>
+        <video className='loginAnimation'>
+
+          <source src={LoginAnimation} type='video/mp4'/>
+          Your browser does not support mp4s.
+        </video>
+
+        <Heading className='loginStart-Heading' lineHeight={1.3}>
+          Smarter choices for a better tomorrow
+        </Heading>
+
+        <Button className='loginStart-Button' backgroundColor={'#6cce58'} color={'#f6f6f6'}>
+          Get started
+        </Button>
+
+        <Button className='loginStart-Button' backgroundColor={'#f6f6f6'} color={'#6cce58'}>
+          Log in
+        </Button>
+
+        <Text className='loginStart-text'>
+          By continuing, you agree to the <span style={{color: "#6cce58"}}>Terms of Service </span> 
+          and <span style={{color: "#6cce58"}}> Privacy Policy</span>
+        </Text>
+      </Stack>
     </div>
   )
 }
