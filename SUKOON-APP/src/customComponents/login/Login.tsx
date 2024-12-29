df8048c3b3bf10feaf7b873ef2cf4c9755fdc7a5
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2024-12-29 03:17:37 +0300

    Added some authorisation logic

--- SUKOON-APP/src/customComponents/login/Login.tsx
+++ SUKOON-APP/src/customComponents/login/Login.tsx
@@ -4,8 +4,30 @@ import './Register.css';
 import { CiLock } from 'react-icons/ci';
 import { MdOutlineEmail } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import React, { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword, AuthError } from 'firebase/auth'
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async(e: React.FormEvent<HTMLFormElement>) => {
+
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(getAuth(), email, password);
+      alert("Logged in successfully");
+    } catch (err) {
+      const errorMessage = (err as AuthError).message;
+      setError(errorMessage);
+    }
+
+  };
+
+
   const navigate = useNavigate(); // Initialize navigate function
 
   // Function to navigate back to Auth page
@@ -16,6 +38,10 @@ const Login = () => {
   const goToQR = () => {
     navigate('/QRWait'); // Navigate to the root Auth page
   };
+
+  const goToRegister = () => {
+    navigate('/register')
+  }
 
   return (
     <div style={{ overflowX: 'hidden' }}>
@@ -57,22 +83,30 @@ const Login = () => {
         </Flex>
 
         <Box className='OuterInputBox'>
-          <Input placeholder="Email" className='InputData' />
+          <Input placeholder="Email" className='InputData' value={email} onChange={(e) => setEmail(e.target.value)}/>
           <Box className='InnerInputBox'>
             <MdOutlineEmail />
           </Box>
         </Box>
 
         <Box className='OuterInputBox'>
-          <Input placeholder="Password" className='InputData' />
+          <Input placeholder="Password" className='InputData' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
           <Box className='InnerInputBox'>
             <CiLock />
           </Box>
         </Box>
 
-        <Button className='next-Button' backgroundColor={'#6cce58'} color={'#f6f6f6'} onClick={goToQR}>
+        <Button className='next-Button' backgroundColor={'#6cce58'} color={'#f6f6f6'} 
+          onClick={(e) => {
+            e.preventDefault(); // Mimics the behavior of form submission
+            handleLogin(e as unknown as React.FormEvent<HTMLFormElement>);
+          }}>
           Next
         </Button>
+
+        <Text className='registeryText'>
+          Don't have an account? <span style={{ color: "#6cce58", textDecoration: 'underline'}} onClick={goToRegister}> Sign Up!</span>
+        </Text>
 
         <div className="buttons-container">
           <div className="apple-login-button">
@@ -88,7 +122,7 @@ const Login = () => {
             >
               <path d="M747.4 535.7c-.4-68.2 30.5-119.6 92.9-157.5-34.9-50-87.7-77.5-157.3-82.8-65.9-5.2-138 38.4-164.4 38.4-27.9 0-91.7-36.6-141.9-36.6C273.1 298.8 163 379.8 163 544.6c0 48.7 8.9 99 26.7 150.8 23.8 68.2 109.6 235.3 199.1 232.6 46.8-1.1 79.9-33.2 140.8-33.2 59.1 0 89.7 33.2 141.9 33.2 90.3-1.3 167.9-153.2 190.5-221.6-121.1-57.1-114.6-167.2-114.6-170.7zm-105.1-305c50.7-60.2 46.1-115 44.6-134.7-44.8 2.6-96.6 30.5-126.1 64.8-32.5 36.8-51.6 82.3-47.5 133.6 48.4 3.7 92.6-21.2 129-63.7z"></path>
             </svg>
-            <span>Continue with Apple</span>
+            <span>Sign in with Apple</span>
           </div>
           <div className="google-login-button">
             <svg
@@ -126,7 +160,7 @@ const Login = () => {
                         c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
               ></path>
             </svg>
-            <span>Continue with Google</span>
+            <span>Sign in with Google</span>
           </div>
         </div>
       </Stack>
@@ -135,3 +169,5 @@ const Login = () => {
 };
 
 export default Login;
+
+
