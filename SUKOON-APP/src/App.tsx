ba3e9c67b61f3478379f63948c7edb802da13bb3
ZaidAljundiHW2 <145095434+ZaidAljundiHW2@users.noreply.github.com>
2025-01-17 10:01:21 +0400

    started homepage

--- SUKOON-APP/src/App.tsx
+++ SUKOON-APP/src/App.tsx
@@ -3,14 +3,13 @@ import { Route, Routes } from 'react-router-dom'; // Import Routes and Route
 import Register from '@/customComponents/login/register.tsx';
 import Auth from '@/customComponents/login/auth.tsx';
 import Login from '@/customComponents/login/Login.tsx';
+import Homepage from '@/customComponents/homepage/Homepage.tsx'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Auth />} />         {/* Home or Auth page */}
-      <Route path="/login" element={<Login />} />   {/* Login page */}
-      <Route path="/register" element={<Register />} /> {/* Register page */}
-    </Routes>
+    <Homepage>
+      
+    </Homepage>
   );
 };
 
---
+++ SUKOON-APP/src/customComponents/homepage/Homepage.tsx
@@ -0,0 +1,21 @@
+import { Box, Heading, Stack } from '@chakra-ui/react'
+import React from 'react'
+import './Homepage.css'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Stack className='homepageContainer'>
+        <Box className='homepageHeader'>
+
+          <Heading>
+          </Heading>
+          
+        </Box>
+      </Stack>
+      
+    </div>
+  )
+}
+
+export default Homepage
