d1beda4d3a3edf3909188fb17cbc10cb6275989f
Mufliha Dawood <ms2176@hw.ac.uk>
2025-01-19 23:48:34 +0400

    stats main page

--- SUKOON-APP/src/App.tsx
+++ SUKOON-APP/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'; // Import Routes and Route
 import Register from '@/customComponents/login/register.tsx';
 import Auth from '@/customComponents/login/auth.tsx';
 import Login from '@/customComponents/login/Login.tsx';
+import Statistics from '@/customComponents/stats/stats_mainpage'; // Ensure the correct path to your Statistics component
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Auth />} />         {/* Home or Auth page */}
       <Route path="/login" element={<Login />} />   {/* Login page */}
       <Route path="/register" element={<Register />} /> {/* Register page */}
+      <Route path="/stats" element={<Statistics />} /> {/* Statistics page */}
     </Routes>
   );
 };
